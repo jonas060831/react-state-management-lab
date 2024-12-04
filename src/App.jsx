d3deb1bb47482d8a35ebfde26418ef34ab8f728e
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-12-03 21:45:47 -0800

    downloaded the images and fix the bug of delay state by processing array first before updating the state

--- src/App.jsx
+++ src/App.jsx
@@ -22,70 +22,70 @@ const App = () => {
       price: 12,
       strength: 6,
       agility: 4,
-      img: 'https://via.placeholder.com/150/92c952',
+      img: 'images/92c952.png',
     },
     {
       name: 'Scavenger',
       price: 10,
       strength: 5,
       agility: 5,
-      img: 'https://via.placeholder.com/150/771796',
+      img: 'images/771796.png',
     },
     {
       name: 'Shadow',
       price: 18,
       strength: 7,
       agility: 8,
-      img: 'https://via.placeholder.com/150/24f355',
+      img: 'images/24f355.png',
     },
     {
       name: 'Tracker',
       price: 14,
       strength: 7,
       agility: 6,
-      img: 'https://via.placeholder.com/150/d32776',
+      img: 'images/d32776.png',
     },
     {
       name: 'Sharpshooter',
       price: 20,
       strength: 6,
       agility: 8,
-      img: 'https://via.placeholder.com/150/1ee8a4',
+      img: 'images/1ee8a4.png',
     },
     {
       name: 'Medic',
       price: 15,
       strength: 5,
       agility: 7,
-      img: 'https://via.placeholder.com/150/66b7d2',
+      img: 'images/66b7d2.png',
     },
     {
       name: 'Engineer',
       price: 16,
       strength: 6,
       agility: 5,
-      img: 'https://via.placeholder.com/150/56acb2',
+      img: 'images/56acb2.png',
     },
     {
       name: 'Brawler',
       price: 11,
       strength: 8,
       agility: 3,
-      img: 'https://via.placeholder.com/150/8985dc',
+      img: 'images/8985dc.png',
     },
     {
       name: 'Infiltrator',
       price: 17,
       strength: 5,
       agility: 9,
-      img: 'https://via.placeholder.com/150/392537',
+      img: 'images/392537.png',
     },
     {
       name: 'Leader',
       price: 22,
       strength: 7,
       agility: 6,
-      img: 'https://via.placeholder.com/150/602b9e',
+      img: 'images/602b9e.png',
     },
   ])
   
@@ -93,94 +93,95 @@ const App = () => {
   // 3. Create a function named handleAddFighter.
   //This function will be triggered when you click the Add button for any character
   //in the zombieFighters list
-  const handleAddFighter = (selectedCharacter) => {
+  // const handleAddFighter = (selectedCharacter) => {
 
-    //3.3 Before adding a character to the team, check if you have enough money to afford them.
-    //If your money is less than the character’s price,
-    //you shouldn’t be able to add them. In such cases,
-    //log a message to the console such as "Not enough money"
-    if(money < selectedCharacter.price) {
-      console.log("Not enough money")
-    } else {
+  //   //3.3 Before adding a character to the team, check if you have enough money to afford them.
+  //   //If your money is less than the character’s price,
+  //   //you shouldn’t be able to add them. In such cases,
+  //   //log a message to the console such as "Not enough money"
+  //   if(money < selectedCharacter.price) {
+  //     console.log("Not enough money")
+  //   } else {
 
-      //3.1 When you click Add on a character, this function should add the selected character’s object
-      //to the team state array. This is how you build your team.
-      setTeam([...team, selectedCharacter])
-      //3.2 Each character comes with a price. Upon adding a character to your team,
-      //subtract the character’s price from your current money value.
-      //Think of it as spending money to recruit a team member.
-      setMoney(prevValue => {
-        return prevValue - selectedCharacter.price
-      })
-    } 
-  }
+  //     //3.1 When you click Add on a character, this function should add the selected character’s object
+  //     //to the team state array. This is how you build your team.
+  //     setTeam([...team, selectedCharacter])
+  //     //3.2 Each character comes with a price. Upon adding a character to your team,
+  //     //subtract the character’s price from your current money value.
+  //     //Think of it as spending money to recruit a team member.
+  //     setMoney(prevValue => {
+  //       return prevValue - selectedCharacter.price
+  //     })
+  //   } 
+  // }
 
   // 5.2 Whenever a character is added or removed from the team, recalculate the total strength.
   //This calculation should sum up the strength values of all characters currently in the team.
   //(A great case for a helper function!)
-  const recalculateTotalStrength = (member, isBeingRemove=false) => {
+  const recalculateTotalStrength = (team) => {
     
+    //reduce
+    const newStrength =  team.reduce((accumulator, m) => accumulator + m.strength , 0)
     
-    //5.4 I added a functionality where the strength should not be increased if money is not enough for the team member price
-    if(money < member.price) {
-      console.log('Not enough money for this member')
-    } else {
-
-      //5.3we wanted to sum the characters strength and store it to a variable
-      //ill try using reducer and initialize the value to 0
-      //reducer test fails and using the previous state is the correct answer
-      setTotalStrength(prevState => {
-
-        //if isBeingRemove is true then reflect it to the ui by deducting from total strength
-        if(isBeingRemove) return prevState -= member.strength
-
-        return prevState += member.strength
-      })
-    }
+    setTotalStrength(newStrength)
   }
 
   //6.2 Just like with strength, recalculate total agility whenever there’s a change in the team.
   //This should be the sum of the agility values of all the team members.
-  const recalculateTotalAgility = (member, isBeingRemove=false) => {
+  const recalculateTotalAgility = (team) => {
 
-    if(money < member.price) {
-      console.log('Not enough money for this member')
+    //reduce
+    const newAgility =  team.reduce((accumulator, m) => accumulator + m.agility , 0)
+    
+    setTotalAgility(newAgility)
+  }
+
+  const handleAddFighter = (fighter) => {
+
+    if(money >= fighter.price) {
+      //using the spread operator add the new fighter to the team variable
+      //make it a habit to do this
+      const updatedTeam = [...team, fighter]
+      //and then update the state
+      setTeam(updatedTeam)
+
+      setMoney(money - fighter.price)
+
+      recalculateTotalStrength(updatedTeam)
+      recalculateTotalAgility(updatedTeam)
     } else {
-      setTotalAgility(prevState => {
-
-        if (isBeingRemove) return prevState -= member.agility
-        return prevState += member.agility
-      })
+      console.log('Not enough money')
     }
   }
-
-  const handleAddTeamMember = (fighter) => {
-    handleAddFighter(fighter)
-    recalculateTotalStrength(fighter)
-    recalculateTotalAgility(fighter)
-  }
-
   // 8. Create a function named handleRemoveFighter. This handler function is key to managing your team. This function enables you to remove characters, adjusting the total strength, agility, and budget of your team accordingly.
-  const handleRemoveFighter = (fighter, index) => {
+  
+  //8.5 Ensure that the UI reflects the removal of the character from your team. This includes updates to the total strength and agility displays, and the available budget.
+  const handleRemoveFighter = ( fighterIndex) => {
     
     //8.2 This function should determine which character needs to be removed based on user
     //interaction (usually, this is passed via an identifier like an ID or an index in the array).
     //sorry :( i shortcut and add the index to the parameter of the function
     //I didnt realize i should reference the whole zombieFighter array
-    setTeam(prevTeam => {
-      //8.3 Once the character to be removed is identified,
-      //the team state should be updated to exclude this character.
-      //This can be achieved by creating a new array that filters out the selected character.
-      return prevTeam.filter( (member, memberIndex) => index !== memberIndex )
-    })
-    //8.4 Increase the money state by the price of the removed character, effectively refunding the cost to your budget.
-    setMoney(prevMoney => {
-      return prevMoney += fighter.price
-    })
+    // setTeam(prevTeam => {
+    //   //8.3 Once the character to be removed is identified,
+    //   //the team state should be updated to exclude this character.
+    //   //This can be achieved by creating a new array that filters out the selected character.
+    //   return prevTeam.filter( (member, memberIndex) => index !== memberIndex )
+    // })
 
-    recalculateTotalStrength(fighter, true)
-    recalculateTotalAgility(fighter, true)
-    
+    //exclude the fighter by it index in the team state
+    //i need to make this a habit when im deleting something from the array to use filter
+    const newTeam = team.filter( (_, index) => index !== fighterIndex)
+    setTeam(newTeam)
+
+    //8.4 Increase the money state by the price of the removed character, effectively refunding the cost to your budget.
+    //since we have the fighterIndex is equivalent to array index of team we can acces the object of thing that we are trying to remove{}
+    const fighterCost = team[fighterIndex].price
+    //add it back to the pool of money
+    setMoney(money + fighterCost)
+
+    recalculateTotalStrength(newTeam)
+    recalculateTotalAgility(newTeam)
   }
 
   return (
@@ -228,7 +229,7 @@ const App = () => {
                   This button, when clicked, should call a handler function to remove the
                   character from your team. */}
                   {/* 8.1 this function will be executed when you click the remove button for a character in your team */}
-                  <button onClick={() => handleRemoveFighter(member, index)}>Remove</button>
+                  <button onClick={() => handleRemoveFighter(index)}>Remove</button>
                 </li>
               ))
             }
@@ -249,7 +250,7 @@ const App = () => {
               Strength: {fighter.strength} <br />
               Agility: {fighter.agility} <br />
 
-              <button onClick={() => handleAddTeamMember(fighter) }>Add</button>
+              <button onClick={() => handleAddFighter(fighter) }>Add</button>
             </li>
           ))
         }
